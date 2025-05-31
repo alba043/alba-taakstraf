a00e3aedecbe10e248927fb11b85af84a17b2bb0
alba043 <jasper04512@gmail.com>
2025-05-31 15:24:23 +0200

    Script + Database Gemaakt

---
+++ locales/en.lua
@@ -0,0 +1,7 @@
+Locales ['en'] = {
+    ['judge']               = 'RECHTER',
+    ['escape_attempt']      = 'Je kunt niet ontsnappen. Je taakstraf is verlengd.',
+    ['remaining_msg']       = 'Je moet nog ~b~%s~s~ acties voltooien voordat je taakstraf is afgerond.',
+    ['comserv_msg']         = '%s is veroordeeld tot %s maanden taakstraf.',
+    ['comserv_finished']    = '%s heeft zijn taakstraf voltooid!'
+}
